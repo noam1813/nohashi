34fb56c3f5333d8b6861eecc78ad9d4a1dabf0bb
Noam1813 <13.noam.18@gmail.com>
2020-10-04 18:18:01 +0900

    UIベースの作成、Managerの追加

---
+++ PacmanLike/Assets/TimeManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[Serializable]
+public class FazeData
+{
+    
+}
+
+[Serializable]
+public enum TimeZoneData
+{
+    Noon,Night    
+}
+public class TimeManager : MonoBehaviour
+{
+    [SerializeField] private int day;
+
+    [SerializeField] private TimeZoneData timeZone;
+
+    [SerializeField] private int time;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
