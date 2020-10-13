f72fc7e1d0ba3fd482a782441b931c933533b528
kohak <kohak213@gmail.com>
2020-10-13 23:20:48 +0900

    カメラがのはしを追従するように

---
+++ PacmanLike/Assets/Scripts/CameraManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    public GameObject Player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(5,5,-10);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        transform.position = new Vector3(Player.transform.position.x,Player.transform.position.y,-10);
+    }
+}
