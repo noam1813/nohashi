e8432a7133d931469eda73c13c91ad655f29f973
Seino544 <s.seino.544@ms.saitama-u.ac.jp>
2020-10-17 00:20:55 +0900

    どのシーンからでもfaderを呼び出せるように変更

---
+++ PacmanLike/Assets/Scripts/SceneTransitionManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class SceneTransitionManager : MonoBehaviour
+{
+    public static SceneTransitionManager Instance;
+
+    public enum SceneTitle
+    {
+        Title,
+        ChapterSelect,
+        Stage1_1,
+        Stage1_2,
+        Stage1_3,
+        Clear,
+        GameOver
+    }
+
+    void Start()
+    {
+        Instance = GetComponent<SceneTransitionManager>();
+    }
+
+    void Update()
+    {
+
+    }
+
+    public IEnumerator SceneTransitionToPlay(string sceneTitle, float waitaTime)
+    {
+        yield return new WaitForSeconds(waitaTime);
+        SceneManager.LoadScene(sceneTitle);
+
+    }
+    public void OnButtonClick()
+    {
+        StartCoroutine(SceneTransitionManager.Instance.SceneTransitionToPlay("ChapterSelect", 0));
+    }
+}
