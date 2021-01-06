c0a2939abc0bd774d69d009da8093a9031e4e59e
Noam1813 <13.noam.18@gmail.com>
2021-01-07 02:09:40 +0900

    ポーズ画面にScreenWith適用

--- PacmanLike/Assets/Scripts/MainGameScene/MainGameMusicManager.cs
+++ PacmanLike/Assets/Scripts/MainGameScene/MainGameMusicManager.cs
@@ -176,7 +176,7 @@ public class MainGameMusicManager : MonoBehaviour
         {
             noonBGM.volume = 0f;
             nightBGM.volume = 0f;
-            yield return new WaitForSeconds(1f);
+            //yield return new WaitForSeconds(1f);
             battleBGM.Play();
         }
         else
@@ -200,5 +200,7 @@ public class MainGameMusicManager : MonoBehaviour
                     )
                 );
         }
+
+        yield return null;
     }
 }
