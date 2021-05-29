f1c2ddb4caf5406306b09569450cf33e1176b127
Akifumi Yao <haru.eightdoublezero@gmail.com>
2021-05-29 15:47:45 +0900

    プレイ画面の水エフェクト追加 プレイ画面の背景追加 タイトル画面のUIサイズ変更

---
+++ PacmanLike/Assets/Scripts/WaveEffectMover.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveEffectMover : MonoBehaviour
+{
+    public float waveSpeed = 1;
+    private Vector3 position;
+    private float x;
+    private float time = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        position = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime * waveSpeed;
+        x = Mathf.Sin(time);
+        transform.position = position + new Vector3(x, 0, 0);
+    }
+}
